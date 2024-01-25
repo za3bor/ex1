33030df2ec2d0ae7a125296201437f62d657d904
za3bor <fadisrour.za3bor@gmail.com>
2024-01-25 15:16:11 +0200

    add fuc

--- set_git.bat
+++ set_git.bat
@@ -8,5 +8,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/[set your github repository path].git
+git remote add origin https://github.com/za3bor/ex1.git
 git push -u origin main
